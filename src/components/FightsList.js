68c71213f247ce5f23eabf3ab83c4594cf25882c
Maximiliano Pinto <maximilianopinto95@gmail.com>
2021-08-19 04:22:01 -0300

    New navbar, loading animation

--- src/components/FightsList.js
+++ src/components/FightsList.js
@@ -27,7 +27,8 @@ const limitFightsList = (zoanId, arraySize) => {
 export default class FightsList extends Component {
 
     state = {
-        fights: []
+        fights: [],
+        loaded: true
     }
 
     async componentDidMount() {
@@ -37,12 +38,16 @@ export default class FightsList extends Component {
 
     getFights = async () => {
         if (this.props.match.params.zoan_id != null) {
+            this.setState({ loaded: false });
             const res = await axios.get('https://gestor-cryptozoon.herokuapp.com/api/fights/byzoan/' + this.props.match.params.zoan_id);
             this.setState({ fights: res.data });
+            this.setState({ loaded: true });
         }
         else {
+            this.setState({ loaded: false });
             const res = await axios.get('https://gestor-cryptozoon.herokuapp.com/api/fights');
             this.setState({ fights: res.data });
+            this.setState({ loaded: true });
         }
     }
 
@@ -56,41 +61,48 @@ export default class FightsList extends Component {
 
     render() {
         return (
-            <div className="container">
-                <CSVLink filename={"Fights_List.csv"} data={this.state.fights} className="btn btn-warning btn-4 bi bi-cloud-arrow-down-fill"> Export to CSV </CSVLink>
-                <div className="table-responsive-sm">
-                    <table className="table table-dark table-hover table-bordered">
-                        <thead>
-                            <tr>
-                                <th><div style={{ textAlign: "center" }}>Date</div></th>
-                                <th><div style={{ textAlign: "center" }}>Zoon</div></th>
-                                <th><div style={{ textAlign: "center" }}>Exp</div></th>
-                                <th><div style={{ textAlign: "center" }}>Result</div></th>
-                                <th><div style={{ textAlign: "center" }}>Fee</div></th>
-                                <th><div style={{ textAlign: "center" }}>Monster</div></th>
-                                <th><div style={{ textAlign: "center" }}>Actions</div></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.fights.slice(limitFightsList(this.props.match.params.zoan_id, this.state.fights.length), this.state.fights.length).map(fight => {
-                                    return (
-                                        <tr key={fight._id}>
-                                            <td><div style={{ textAlign: "center", margin: '10px' }}>{moment(fight.date).format("LLL")}</div></td>
-                                            <td><div style={{ textAlign: "center", margin: '10px' }}>{fight.zoon}</div></td>
-                                            <td><div style={{ textAlign: "center", margin: '10px' }}>{fight.exp}</div></td>
-                                            <td><div style={{ textAlign: "center" }}><button className={getResultIcon(fight.result)}>{fight.result}</button></div></td>
-                                            <td><div style={{ textAlign: "center", margin: '10px' }}>{fight.fee}</div></td>
-                                            <td><div style={{ textAlign: "center", margin: '10px' }}>{fight.monster}</div></td>
-                                            <td><div style={{ textAlign: "center" }}><button onClick={() => this.deleteFight(fight._id)} className="btn btn-warning bi bi-trash">Delete</button></div></td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
+            this.state.loaded ?
+                <div className="container">
+                    <CSVLink filename={"Fights_List.csv"} data={this.state.fights} className="btn btn-warning btn-4 bi bi-cloud-arrow-down-fill"> Export to CSV </CSVLink>
+                    <div className="table-responsive-sm">
+                        <table className="table table-dark table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                    <th><div style={{ textAlign: "center" }}>Date</div></th>
+                                    <th><div style={{ textAlign: "center" }}>Zoon</div></th>
+                                    <th><div style={{ textAlign: "center" }}>Exp</div></th>
+                                    <th><div style={{ textAlign: "center" }}>Result</div></th>
+                                    <th><div style={{ textAlign: "center" }}>Fee</div></th>
+                                    <th><div style={{ textAlign: "center" }}>Monster</div></th>
+                                    <th><div style={{ textAlign: "center" }}>Actions</div></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.fights.slice(limitFightsList(this.props.match.params.zoan_id, this.state.fights.length), this.state.fights.length).map(fight => {
+                                        return (
+                                            <tr key={fight._id}>
+                                                <td><div style={{ textAlign: "center", margin: '10px' }}>{moment(fight.date).format("LLL")}</div></td>
+                                                <td><div style={{ textAlign: "center", margin: '10px' }}>{fight.zoon}</div></td>
+                                                <td><div style={{ textAlign: "center", margin: '10px' }}>{fight.exp}</div></td>
+                                                <td><div style={{ textAlign: "center" }}><button className={getResultIcon(fight.result)}>{fight.result}</button></div></td>
+                                                <td><div style={{ textAlign: "center", margin: '10px' }}>{fight.fee}</div></td>
+                                                <td><div style={{ textAlign: "center", margin: '10px' }}>{fight.monster}</div></td>
+                                                <td><div style={{ textAlign: "center" }}><button onClick={() => this.deleteFight(fight._id)} className="btn btn-warning bi bi-trash">Delete</button></div></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
+                :
+                <div class="d-flex justify-content-center">
+                    <div class="spinner-border text-warning m-5" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
         )
     }
 }
